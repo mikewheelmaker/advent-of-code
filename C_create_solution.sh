edf1514921f8d84fbc470a66ff0daab91c14135e
Mike Wheelmakersson <radumihairotariu@gmail.com>
2025-01-02 14:02:59 +0200

    Improved C build scripts
    Added possibility to use lssl and lcrypto as command line arguments to build solutions that require openssl library (for example 2015_04).
    Disabled printing of commands in Windows build script.

--- C_create_solution.sh
+++ C_create_solution.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-gcc "$1/functions.c" "$1/main.c" -o "$1/solution"
+if [ "$2" == "lssl" ]; then
+    if [ "$3" == "lcrypto" ]; then
+        gcc "$1/functions.c" "$1/main.c" -$2 -$3 -o "$1/solution"
+    fi
+else
+    gcc "$1/functions.c" "$1/main.c" -o "$1/solution"
+fi
 cd $1
 ./solution
 cd ../../..
--- C_create_solution_exe.bat
+++ C_create_solution_exe.bat
@@ -1,4 +1,11 @@
-gcc "%1/functions.c" "%1/main.c" -o "%1/solution.exe"
+@echo off
+IF "%2" == "lssl" (
+    IF "%3" == "lcrypto" (
+        gcc "%1/functions.c" "%1/main.c" -%2 -%3 -o "%1/solution.exe"
+    )
+) ELSE (
+    gcc "%1/functions.c" "%1/main.c" -o "%1/solution.exe"
+)
 cd %1
 solution.exe
 cd ../../..
