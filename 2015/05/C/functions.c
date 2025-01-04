fa1514e42d593d275d8a84f4405f19ccd6fb229c
Mike Wheelmakersson <radumihairotariu@gmail.com>
2025-01-04 16:49:58 +0200

    2015_05 C solution
    Implemented solution in C for day 5, year 2015.

---
+++ 2015/05/C/functions.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "functions.h"
+
+bool p1_condition1(char *string)
+{
+    char vowels[] = "aeiou";
+    char *pointerToCharacter;
+    int numberOfVowelsPresent = 0;
+    pointerToCharacter = strpbrk(string, vowels);
+    while (pointerToCharacter != NULL)
+    {
+        ++numberOfVowelsPresent;
+        pointerToCharacter = strpbrk(pointerToCharacter + 1, vowels);
+    }
+    if(numberOfVowelsPresent >= 3)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool p1_condition2(char *string)
+{
+    int i;
+    for(i = 0; i < strlen(string) - 1; ++i)
+    {
+        if(string[i] == string[i+1])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool p1_condition3(char *string)
+{
+    if(strstr(string, "ab") != NULL)
+    {
+        return false;
+    }
+    if(strstr(string, "cd") != NULL)
+    {
+        return false;
+    }
+    if(strstr(string, "pq") != NULL)
+    {
+        return false;
+    }
+    if(strstr(string, "xy") != NULL)
+    {
+        return false;
+    }
+    return true;
+}
+
+int first_puzzle(char *fileName)
+{
+    FILE *filePointer = fopen(fileName, "r");
+    if(filePointer != NULL)
+    {
+        printf("Input file is open for reading.\n");
+        char str[17];
+        int total = 0;
+        do
+        {
+            fgets(str, 17, filePointer);
+            if(p1_condition1(str) && p1_condition2(str) && p1_condition3(str))
+            {
+                ++total;
+            }
+        } while (!feof(filePointer));
+        return total;
+    }
+    else
+    {
+        printf("Could not open the input file for reading.\n");
+    }
+
+    fclose(filePointer);
+    return -1;
+}
+
+bool p2_triplets(char *string)
+{
+    int i;
+    for(i = 0; i < strlen(string) - 2; ++i)
+    {
+        if(string[i] == string[i + 1] && string[i] == string[i + 2])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool p2_condition1(char *string)
+{
+    int i;
+    for(i = 0; i < strlen(string) - 1; ++i)
+    {
+        char subString[3];
+        subString[0] = string[i];
+        subString[1] = string[i + 1];
+        subString[2] = '\0';
+        char *pointerToSubString = strstr(string + i + 1, subString);
+        if(pointerToSubString != NULL)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+bool p2_condition2(char *string)
+{
+    int i;
+    for(i = 0; i < strlen(string) - 2; ++i)
+    {
+        if(string[i] == string[i + 2] && string[i] != string[i + 1])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int second_puzzle(char *fileName)
+{
+    FILE *filePointer = fopen(fileName, "r");
+    if(filePointer != NULL)
+    {
+        printf("Input file is open for reading.\n");
+        char str[17];
+        int total = 0;
+        do
+        {
+            fgets(str, 17, filePointer);
+            if(p2_condition1(str) && !p2_triplets(str) && p2_condition2(str))
+            {
+                printf("Nice string found: %s\n", str);
+                ++total;
+            }
+        } while (!feof(filePointer));
+        return total;
+    }
+    else
+    {
+        printf("Could not open the input file for reading.\n");
+    }
+
+    fclose(filePointer);
+    return -1;
+}
---
+++ 2015/05/C/functions.h
@@ -0,0 +1,19 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <stdbool.h>
+
+#define SIMPLE_INPUT_FILE_PART_1 "../simple_input_part_1.txt"
+#define SIMPLE_INPUT_FILE_PART_2 "../simple_input_part_2.txt"
+#define INPUT_FILE "../input.txt"
+
+bool p1_condition1(char *string);
+bool p1_condition2(char *string);
+bool p1_condition3(char *string);
+int first_puzzle(char *fileName);
+bool p2_triplets(char *string);
+bool p2_condition1(char *string);
+bool p2_condition2(char *string);
+int second_puzzle(char *fileName);
+
+#endif
