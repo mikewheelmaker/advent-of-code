24904ea4b8f52223c0d5a7cd11e870f6066a0b26
Mike Wheelmakersson <radumihairotariu@gmail.com>
2025-03-05 10:22:10 +0200

    2015_08 C solution
    Implemented solution in C for day 8, year 2015.

---
+++ 2015/08/C/functions.c
@@ -0,0 +1,138 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "functions.h"
+
+int getNumOfMemoryChars(char *str)
+{
+    int result = 0;
+    int i;
+    for(i = 0; i < strlen(str); ++i)
+    {
+        if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            ++result;
+        }
+        if(str[i] == '\\')
+        {
+            ++i;
+            if(str[i] != 'x')
+            {
+                ++result;
+            }
+            else
+            {
+                i += 2;
+                ++result;
+            }
+        }
+    }
+    return result;
+}
+
+int first_puzzle(char *fileName)
+{
+    FILE *pFile = fopen(fileName, "r");
+    if(pFile != NULL)
+    {
+        printf("Input file is open for reading.\n");
+        char str[50];
+        int totalNumOfCodeChars = 0;
+        int totalNumOfMemoryChars = 0;
+        do
+        {
+            fgets(str, 50, pFile);
+            if(feof(pFile))
+            {
+                break;
+            }
+            if(strlen(str) > 2)
+            {
+                int i;
+                for(i = 0; i < strlen(str); ++i)
+                {
+                    if(str[i] == '\n')
+                    {
+                        str[i] = '\0';
+                    }
+                }
+            }
+            totalNumOfCodeChars += strlen(str);
+            totalNumOfMemoryChars += getNumOfMemoryChars(str);
+        } while (!feof(pFile));
+        return totalNumOfCodeChars - totalNumOfMemoryChars;
+    }
+    else
+    {
+        printf("Could not open the input file for reading.\n");
+    }
+
+    fclose(pFile);
+    return -1;
+}
+
+void encode(char *str, char *dest)
+{
+    int i;
+    int j = 0;
+    dest[j++] = '\"';
+    for(i = 0; i < strlen(str); ++i)
+    {
+        if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= '0' && str[i] <= '9'))
+        {
+            dest[j++] = str[i];
+        }
+        if(str[i] == '\"' || str[i] == '\\')
+        {
+            dest[j++] = '\\';
+            dest[j++] = str[i];
+        }
+    }
+    dest[j++] = '\"';
+    dest[j] = '\0';
+}
+
+int second_puzzle(char *fileName)
+{
+    FILE *pFile = fopen(fileName, "r");
+    if(pFile != NULL)
+    {
+        printf("Input file is open for reading.\n");
+        char str[50];
+        int totalNumOfCodeChars = 0;
+        int totalNumOfMemoryChars = 0;
+        do
+        {
+            fgets(str, 50, pFile);
+            if(feof(pFile))
+            {
+                break;
+            }
+            if(strlen(str) > 2)
+            {
+                int i;
+                for(i = 0; i < strlen(str); ++i)
+                {
+                    if(str[i] == '\n')
+                    {
+                        str[i] = '\0';
+                    }
+                }
+            }
+            char *tmp = (char *)malloc(100 * sizeof(char));
+            encode(str, tmp);
+            totalNumOfCodeChars += strlen(tmp);
+            totalNumOfMemoryChars += strlen(str);
+            free(tmp);
+        } while (!feof(pFile));
+        return totalNumOfCodeChars - totalNumOfMemoryChars;
+    }
+    else
+    {
+        printf("Could not open the input file for reading.\n");
+    }
+
+    fclose(pFile);
+    return -1;
+}
---
+++ 2015/08/C/functions.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <stdio.h>
+
+#define SIMPLE_INPUT_FILE "../simple_input.txt"
+#define INPUT_FILE "../input.txt"
+
+int getNumOfMemoryChars(char *str);
+int first_puzzle(char *fileName);
+void encode(char *str, char *dest);
+int second_puzzle(char *fileName);
+
+#endif
